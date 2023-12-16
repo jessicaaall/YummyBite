e23fd0b681c1a8dca2da9e2f81ebecfeeb546166
unknown <18221097@std.stei.itb.ac.id>
2023-12-16 14:19:15 +0700

    finalisasi

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,17 +5,16 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'HomeController::index', ['filter' => 'authGuard']);
 $routes->get('/pemilik-restoran', 'PemilikRestoranController::index');
 $routes->get('/restoranAPI/?(:any)?', 'RestoranController::index/$1');
 $routes->get('/restoranbyid/(:any)', 'RestoranController::restoranbyId/$1');
 $routes->get('/makananAPI/(:any)', 'MakananController::index/$1');
 $routes->get('/login', 'AuthController::index');
 $routes->match(['get', 'post'], 'AuthController/loginAuth', 'AuthController::loginAuth');
-$routes->get('/logout', 'AuthController::logout');
+$routes->get('/logout', 'AuthController::logout', ['filter' => 'authGuard']);
 
-$routes->get('/transaction','TransactionController::pemesanan', ['filter' => 'authGuard']);
-$routes->get('/menulist','MenuListController::menu', ['filter' => 'authGuard']);
+$routes->get('/transaction', 'TransactionController::pemesanan', ['filter' => 'authGuard']);
+$routes->get('/menulist', 'MenuListController::menu', ['filter' => 'authGuard']);
 $routes->get('/addmenu', 'MenuListController::addmenu', ['filter' => 'authGuard']);
 $routes->get('/editmenu/(:any)', 'MenuListController::editmenu/$1', ['filter' => 'authGuard']);
 $routes->match(['get', 'post'], 'MenuListController/insertMenu', 'MenuListController::insertMenu', ['filter' => 'authGuard']);
